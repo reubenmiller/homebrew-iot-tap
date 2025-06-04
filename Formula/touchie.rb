b9cd4007f2022c8b56c8c52addfed64ad6bec490
Reuben Miller <3029781+reubenmiller@users.noreply.github.com>
2025-06-04 19:05:51 +0000

    touchie: update 0.0.4 bottle.

--- Formula/touchie.rb
+++ Formula/touchie.rb
@@ -6,9 +6,10 @@ class Touchie < Formula
   license "MIT"
 
   bottle do
-    root_url "https://github.com/reubenmiller/homebrew-iot-tap/releases/download/touchie-0.0.3"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "83c4675c91d9ad0d853ceb33ddb087f62200663fa75247a39f8a7dd61465ce06"
-    sha256 cellar: :any_skip_relocation, ventura:       "89bf991024e76f7126b73e220d736c61521fcbdef89577941d31ba49f96bdc26"
+    root_url "https://github.com/reubenmiller/homebrew-iot-tap/releases/download/touchie-0.0.4"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "686e36c5d7d2a7a3a54c97f0b356906dc7b33a8fb298126295adaf8cc4920c4c"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5f319d9c20966da3d76e8ff664e771cb19172734866b651ec8fe1c8878fe1839"
+    sha256 cellar: :any_skip_relocation, ventura:       "fc221b70cd9989bddd8d66d84b47f901e5cd92b6b8e06cd6cd0caa602bb2264c"
   end
   depends_on :macos
   uses_from_macos "swift" => :build, since: :sonoma # swift 5.10+
