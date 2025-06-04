18374a96c878e8a2d8a61d8222e35fe0d04fa13c
reubenmiller <reuben.d.miller@gmail.com>
2025-06-04 19:29:27 +0200

    touchie 0.3.0 (new formula)

---
+++ Formula/touchie.rb
@@ -0,0 +1,16 @@
+class Touchie < Formula
+  desc "TouchID access to Mac Keychain via CLI"
+  homepage "https://github.com/reubenmiller/touchie"
+  url "https://github.com/reubenmiller/touchie/archive/0.0.3.tar.gz"
+  sha256 "b6753bfd098598d2a6784d95e15b80509ad644191d6c50e5712dc5d373559dd5"
+  license "MIT"
+
+  def install
+    system "swiftc", "touchie.swift", "-o", "touchie"
+    bin.install "touchie"
+  end
+
+  test do
+    assert_match "no Github token found", shell_output("#{bin}/touchie --help", 255)
+  end
+end
