2eba850681d785dcda9b52bf6f357a315b5fdd84
reubenmiller <reuben.d.miller@gmail.com>
2025-07-08 22:45:54 +0200

    tedge-oscar 0.0.1 (new formula)

---
+++ Formula/tedge-oscar.rb
@@ -0,0 +1,22 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class TedgeOscar < Formula
+  desc "Experiment/research in using OCI artifacts for managing some thin-edge.io artifacts"
+  homepage "https://github.com/reubenmiller/tedge-oscar"
+  url "https://github.com/reubenmiller/tedge-oscar/archive/v0.0.1.tar.gz"
+  sha256 "a9edb0211d1bf2fe54eb95dea0ef8723ce7838a48e3c5020558954107b143a35"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["CGO_ENABLED"] = OS.mac? ? "1" : "0"
+    system "go", "build", *std_go_args(ldflags: "-s -w -X main.version=v#{version} -X main.commit=#{Utils.git_head} -X main.date=#{time.iso8601} -X main.builtBy=homebrew") main.go
+    generate_completions_from_executable(bin/"tedge-oscar", "completion", shells: [:zsh])
+  end
+
+  test do
+    assert_match "Usage:", shell_output("#{bin}/tedge-oscar --help")
+  end
+end
