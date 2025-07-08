123af0c3798ac5178bb8bef1cc76667215cca61c
reubenmiller <reuben.d.miller@gmail.com>
2025-07-08 23:06:30 +0200

    checkout project rather than downloading source code
    Closes #4.
    
    Signed-off-by: Reuben Miller <3029781+reubenmiller@users.noreply.github.com>

--- Formula/tedge-oscar.rb
+++ Formula/tedge-oscar.rb
@@ -1,15 +1,23 @@
 class TedgeOscar < Formula
   desc "Experiment in using OCI artifacts for managing some thin-edge.io artifacts"
   homepage "https://github.com/reubenmiller/tedge-oscar"
-  url "https://github.com/reubenmiller/tedge-oscar/archive/refs/tags/v0.0.1.tar.gz"
-  sha256 "a9edb0211d1bf2fe54eb95dea0ef8723ce7838a48e3c5020558954107b143a35"
+  url "https://github.com/reubenmiller/tedge-oscar.git",
+      tag:      "v0.0.1",
+      revision: "bf41e5211b784e08ec896435679f7241d1739f9c"
   license "MIT"
 
   depends_on "go" => :build
 
   def install
     ENV["CGO_ENABLED"] = OS.mac? ? "1" : "0"
-    system "go", "build", *std_go_args(ldflags: "-s -w -X main.version=v#{version} -X main.commit=#{Utils.git_head} -X main.date=#{time.iso8601} -X main.builtBy=homebrew"), "main.go"
+    ldflags = %W[
+      -s -w
+      -X main.version=v#{version}
+      -X main.commit=#{Utils.git_short_head(length: 7)}
+      -X main.date=#{time.iso8601}
+      -X main.builtBy=homebrew
+    ]
+    system "go", "build", *std_go_args(ldflags:), "main.go"
     generate_completions_from_executable(bin/"tedge-oscar", "completion", shells: [:zsh])
   end
 
