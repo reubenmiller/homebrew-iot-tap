b2b80575b29add85dd07bbd3e48c4072eb1e2e63
Reuben Miller <3029781+reubenmiller@users.noreply.github.com>
2026-02-23 10:24:41 +0000

    tedge-oscar: update 0.11.2 bottle.

--- Formula/tedge-oscar.rb
+++ Formula/tedge-oscar.rb
@@ -7,10 +7,10 @@ class TedgeOscar < Formula
   head "https://github.com/reubenmiller/tedge-oscar.git", branch: "main"
 
   bottle do
-    root_url "https://github.com/reubenmiller/homebrew-iot-tap/releases/download/tedge-oscar-0.11.1"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a03d2a92c722905cf00419c5d614af5d5d0c733126b456a5619d280e44d3ae65"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f0307112c7f8291f86c688aa8f13f9dd9895928cb634312d520ada9766f2c8d2"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "99c60150e7af531384884e14566fa034c5ed122d27ae9679caa1fb643ce714ab"
+    root_url "https://github.com/reubenmiller/homebrew-iot-tap/releases/download/tedge-oscar-0.11.2"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e162b24ea4ef079763b5334f88f6b528ff5b3d61193a3f506a13f4bab8cd13b4"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "66c5a01788df11a3458c7687eb916e73b9ef0999b71e86fb84ee8ae5ef00196f"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "754007eaf7bbce70bee9996d48286bdac58ef66dec5b3807e0394a9a44354a9a"
   end
 
   depends_on "go" => :build
